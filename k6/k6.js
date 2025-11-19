3a73b2a5ab1a5e564dd8b451de56bff8a1af5b47
Phablo Lisboa <phablolisboa01@gmail.com>
2025-11-19 15:38:46 -0300

    fix: issues

--- k6/k6.js
+++ k6/k6.js
@@ -1,43 +1,91 @@
 import http from 'k6/http';
+import { sleep, check } from 'k6';
 import { Trend } from 'k6/metrics';
-import { sleep } from 'k6';
-import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/latest/dist/bundle.js'
+import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/latest/dist/bundle.js';
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 
 
-// métricas custom — um Trend para cada arquitetura
-let mvc = new Trend("mvc_duration");
-let webflux = new Trend("webflux_duration");
-let vthreads = new Trend("vthreads_duration");
+const mvcTrend = new Trend('duration_mvc');
+const webfluxTrend = new Trend('duration_webflux');
+const vthreadsTrend = new Trend('duration_vthreads');
 
-export let options = {
-    vus: 500,          // número de usuários simultâneos
-    duration: '20s',   // duração do teste
+export const options = {
+    scenarios: {
+        mvc_test: {
+            executor: 'constant-vus',
+            vus: 500,
+            duration: '30s',
+            exec: 'testMvc', // Função específica
+        },
+        webflux_test: {
+            executor: 'constant-vus',
+            vus: 500,
+            duration: '30s',
+            exec: 'testWebFlux',
+        },
+        vthreads_test: {
+            executor: 'constant-vus',
+            vus: 500,
+            duration: '30s',
+            exec: 'testVThreads',
+        },
+    },
+
+    thresholds: {
+        'duration_mvc': ['p(95)<2000'],
+        'duration_webflux': ['p(95)<500'],
+        'duration_vthreads': ['p(95)<500'],
+    }
 };
 
-// URLs dos serviços
 const MVC_URL = "http://localhost:8081/user/phablo";
 const WEBFLUX_URL = "http://localhost:8082/user/phablo";
 const VTHREADS_URL = "http://localhost:8083/user/phablo";
 
-export default function () {
-    // teste MVC
-    let r1 = http.get(MVC_URL);
-    mvc.add(r1.timings.duration);
 
+export function testMvc() {
+    const res = http.get(MVC_URL);
+    mvcTrend.add(res.timings.duration);
 
-    // // teste WebFlux
-    let r2 = http.get(WEBFLUX_URL);
-    webflux.add(r2.timings.duration);
-    //
-    // // teste Virtual Threads
-    let r3 = http.get(VTHREADS_URL);
-    vthreads.add(r3.timings.duration);
+    check(res, {
+        'mvc status is 200': (r) => r.status === 200,
+    });
 
-    sleep(1);
+    check(res, {
+        'mvc status is 503': (r) => r.status === 503,
+    });
+}
+
+export function testWebFlux() {
+    const res = http.get(WEBFLUX_URL);
+    webfluxTrend.add(res.timings.duration);
+
+    check(res, {
+        'webflux status is 200': (r) => r.status === 200,
+    });
+
+    check(res, {
+        'webflux status is 503': (r) => r.status === 503,
+    });
+}
+
+export function testVThreads() {
+    const res = http.get(VTHREADS_URL);
+    vthreadsTrend.add(res.timings.duration);
+
+    check(res, {
+        'vthreads status is 200': (r) => r.status === 200,
+    });
+
+    check(res, {
+        'vthreads status is 503': (r) => r.status === 503,
+    });
 }
 
 export function handleSummary(data) {
     return {
-        'summary4.html': htmlReport(data),
-    }
+        'summary_comparativo.html': htmlReport(data),
+        stdout: textSummary(data, { indent: ' ', enableColors: true }),
+    };
 }
+
